55dc4c755b771c79f8c2688f9b57425f1b4d07f6
Ashish Tiwari <ashishtwr314@gmail.com>
2023-05-29 14:14:48 +0530

    Trying to reduce serverless function size prev size: 55.2MB, allowed: 50.0 MB

--- pages/api/htmltopdf.ts
+++ pages/api/htmltopdf.ts
@@ -6,7 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log();
   res.status(200).send(await getData(req.query.url || "https://example.com"));
 }
 
@@ -31,14 +30,7 @@ const getData = async (url) => {
     const page = await browser.newPage();
     await page.setViewport({ width: 2000, height: 1000 });
     await page.goto(url, { waitUntil: "networkidle0" });
-
-    // const items = await page.$eval("main", (element) => {
-    //   return element.innerHTML;
-    // });
-    // await page.setContent(items);
-    // await page.emulateMediaType("screen");
     const pdf = await page.pdf({
-      path: "button.pdf",
       margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
       printBackground: true,
       format: "A4",
